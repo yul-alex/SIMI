8ec43e7f0a749d3a1c6de86e0837483c0af45e04
Yustres <je.yustres@udla.edu.co>
2020-06-21 11:36:43 -0500

    estructura completa

--- app.js
+++ app.js
@@ -2,25 +2,74 @@ let express = require('express')
 let app = express()
 let exphbs = require('express-handlebars')
 let path = require('path')
+
+
 var indexRouter = require('./routes/index');
+var testIM = require('./routes/testIM');
+var testIN = require('./routes/testIN.js');
+var testDI = require('./routes/testDI');
+var bodyParser = require('body-parser');
+var estudiantes = require('./routes/estudiantes');
+var profesores = require('./routes/profesores');
+var todos = require('./routes/todos');
+var editarE = require('./routes/editarE');
+var agregarE = require('./routes/agregarE');
+
 
 
 app.set('views', path.join(__dirname, 'views'))
 app.engine('.hbs', exphbs({
-    defaultLayout: 'main',
-    layoutsDir: path.join(app.get('views'), 'layouts'),
-    partialsDir: path.join(app.get('views'), 'partials'),
-    extname: '.hbs'
-  })
+  defaultLayout: 'main',
+  layoutsDir: path.join(app.get('views'), 'layouts'),
+  partialsDir: path.join(app.get('views'), 'partials'),
+  extname: '.hbs',
+  helpers: {
+    ...helpers,
+    toJSON: (object) => {
+      return JSON.stringify(object);
+    },
+    ifEquals: (arg1, arg2, options) => {
+      return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
+    },
+    formatCurrency: (value) => {
+      return value.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1.");
+    }
+  }
+})
+
 )
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.set('view engine', '.hbs')
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.raw());
+app.use(bodyParser.json());
+
 
 app.use('/', indexRouter);
-app.use('/', require('./routes/index'));
+app.use('/testIM', testIM);
+app.use('/testIN', testIN);
+app.use('/testDI', testDI);
+app.use('/estudiantes', estudiantes);
+app.use('/profesores', profesores);
+app.use('/todos', todos);
+app.use('/editarE', editarE);
+app.use('/agregarE', agregarE);
+
 
 
 
 app.listen(3000, () => {
   console.log("sever start in localhost:3000")
 })
+
+app.use(function (err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
